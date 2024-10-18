ef55f77533decf3b76343503cea4bb3f2a7b78d4
Marcus-Larsson <Mackan2.larsson2008@gmail.com>
2024-10-18 15:04:13 +0200

    Added Player Helaeth

--- Top-Down-Shooter/Assets/Scripts/Player/PlayerDamage.cs
+++ Top-Down-Shooter/Assets/Scripts/Player/PlayerDamage.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class PlayerDamage : MonoBehaviour
 {
+    [SerializeField] int playerHealth = 10;
+    [SerializeField] float invincilityTime = 2f;
+    bool invincible = false;
+
+    void Disableinvincility()
+    {
+        invincible = false;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            Destroy(gameObject);
+            if(invincible == true)
+            {
+                return;
+            }
+            if (playerHealth > 0)
+            {
+                playerHealth--;
+                invincible = true;
+                Invoke("Disableinvincility", invincilityTime);
+                Debug.Log("player Health:" + playerHealth);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
